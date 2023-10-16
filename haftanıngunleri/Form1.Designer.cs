abd496382cbe8d9abe32114d56db9c8fdb39a3ba
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-10-16 15:39:04 +0300

    Add files via upload

---
+++ haftanıngunleri/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace haftanıngunleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmGunler = new System.Windows.Forms.ComboBox();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmGunler
+            // 
+            this.cmGunler.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmGunler.FormattingEnabled = true;
+            this.cmGunler.Items.AddRange(new object[] {
+            "Pazartesi",
+            "Çarşamba",
+            "Perşenbe",
+            "Cuma",
+            "Cumartesi",
+            "Pazar"});
+            this.cmGunler.Location = new System.Drawing.Point(152, 61);
+            this.cmGunler.Name = "cmGunler";
+            this.cmGunler.Size = new System.Drawing.Size(229, 33);
+            this.cmGunler.TabIndex = 0;
+            this.cmGunler.SelectedIndexChanged += new System.EventHandler(this.cmGunler_SelectedIndexChanged);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(62, 122);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(95, 25);
+            this.lblDurum.TabIndex = 1;
+            this.lblDurum.Text = "DURUM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.cmGunler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmGunler;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
+
---
+++ haftanıngunleri/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace haftanıngunleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmGunler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indeks = cmGunler.SelectedIndex;
+
+            if (indeks >= 0 && indeks <= 2)
+            {
+                lblDurum.Text = "Çıkış Saati 16:40";
+            }
+
+            else if (indeks >= 3 && indeks <= 4)
+            {
+                lblDurum.Text = "Çıkış Saati 15:50";
+            }
+            else
+            {
+                lblDurum.Text = "Tatil Günüüü :)";
+            }
+        }
+    }
+}
